528b89b2a409fe0142af7c07d0adaa02ffc90b00
kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-10-20 01:03:48 +0100

    create dynamic selection of state and local government

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template, url_for, request, flash
+from flaskext.mysql import MySQL
+import pymysql.cursors
+import datetime
+import json
+
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_DB'] = 'university'
+app.config['MYSQL_DATABASE_USER'] = 'perception'
+app.config['MYSQL_DATABASE_PASSWORD'] = '$Eunfunmi2021'
+
+mysql = MySQL(app, cursorclass=pymysql.cursors.DictCursor)
+
+
+
+#  # if state_lg[n]["state"] == "Abia":
+#         #     print(state_lg[n]["local"])    
+
+# conn = mysql.get_db()
+# curr = conn.cursor()
+# curr.execute()
+# conn.commit()
+# curr.close()
+
 
 @app.route('/')
 def index():
@@ -9,7 +33,17 @@ def index():
 
 @app.route('/student/portal')
 def portal():
-    return render_template('portal.html')
+        with open('static/json/data.json') as file:
+            content = file.read()
+        state_lg = json.loads(content)
+        states = []
+
+        n = len(state_lg)
+
+        for i in range(n):
+            states.append(state_lg[i]["state"])
+
+        return render_template('portal.html', states=states, state_lg = state_lg, n=n)
 
 
 @app.route('/dashboard')
@@ -20,7 +54,6 @@ def dashboard():
 def students(id):
     id_number = id
     return render_template('students.html', id_number = id_number)
-
 
 
 if __name__ == "__main__":
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,14 @@
-window.onload = function(){
-    
+
+// DYNAMIC SELECTION
+function stateSelect(){
+    var stateValue = document.querySelector('#state-select').value;
+    var options = document.querySelector('#lga').children;
+    for(let i = 0; i < options.length; i++){
+        if(options[i].value == stateValue){
+            options[i].style.display = 'block';
+        } else{
+            options[i].style.display = 'none';
+        }
+    }
 }
+
