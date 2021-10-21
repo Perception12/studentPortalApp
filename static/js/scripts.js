445576e628d1e933e2d953a6db94f51437f4d826
kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2021-10-21 10:29:31 +0100

    add event listeners to radio buttons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
-
-// DYNAMIC SELECTION
-var selectController = (function(){
+var formController = (function(){
+    var gender;
+    // DYNAMIC SELECTION
     document.querySelector('#state-select').addEventListener('change', function(){
         let stateValue = this.value;
         let options = document.querySelector('#lga').children;
@@ -12,65 +12,63 @@ var selectController = (function(){
             }
         }
     });
+
+    //GENDER SELECTION
+    document.querySelector('#male').addEventListener('click', get_gender);
+
+    document.querySelector('#female').addEventListener('click', get_gender);
+
+    function get_gender(){
+        gender = this.value;
+        console.log(gender);
+    }
 })();
-
-// GENDER SELECTION
-
-var gender;
-function selectMaleGender(){
-    gender = document.querySelector('#male').value;
-}
-
-function selectFemaleGender(){
-    gender = document.querySelector('#female').value;
-}
-
 
 
 // PORTAL FORM SUBMIT FUNCTION
 
-function selectId(id) {
-    return document.querySelector(id).value;
-}
+// function selectId(id) {
+//     return document.querySelector(id).value;
+// }
 
 
-function submitForm(){
-    let firstName = selectId('#first-name');
-    let middleName = selectId('#middle-name');
-    let lastName = selectId('#last-name');
-    let email = selectId('#email');
-    let dateOfBirth = selectId('#dateOfBirth');
-    let phoneNumber = selectId('#phone-number');
-    let address = selectId('#address');
-    let state = selectId('#state-select');
-    let localGovernment = selectId('#lga');
-    let nextOfKin = selectId('#nextOfKin');
-    let jambScore = selectId('#jambScore');
+// function submitForm(){
+//     let firstName = selectId('#first-name');
+//     let middleName = selectId('#middle-name');
+//     let lastName = selectId('#last-name');
+//     let email = selectId('#email');
+//     let dateOfBirth = selectId('#dateOfBirth');
+//     let phoneNumber = selectId('#phone-number');
+//     let address = selectId('#address');
+//     let state = selectId('#state-select');
+//     let localGovernment = selectId('#lga');
+//     let nextOfKin = selectId('#nextOfKin');
+//     let jambScore = selectId('#jambScore');
 
-    $.ajax({
-        url: '/student/add',
-        type: 'POST',
-        dataType: 'json',
-        data: JSON.stringify({
-            'firstName': firstName,
-            'middleName': middleName,
-            'lastName': lastName,
-            'email': email,
-            'dateOfBirth': dateOfBirth,
-            'gender': gender,
-            'phoneNumber': phoneNumber,
-            'address': address,
-            'state': state,
-            'localGovernment': localGovernment,
-            'nextOfKin': nextOfKin,
-            'jambScore': jambScore
-        }),
-        contentType: 'application/json, charset=UTF-8',
-        success: function(data) {
-            location.reload();
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    });
-}
+//     $.ajax({
+//         url: '/student/add',
+//         type: 'POST',
+//         dataType: 'json',
+//         data: JSON.stringify({
+//             'firstName': firstName,
+//             'middleName': middleName,
+//             'lastName': lastName,
+//             'email': email,
+//             'dateOfBirth': dateOfBirth,
+//             'gender': gender,
+//             'phoneNumber': phoneNumber,
+//             'address': address,
+//             'state': state,
+//             'localGovernment': localGovernment,
+//             'nextOfKin': nextOfKin,
+//             'jambScore': jambScore
+//         }),
+//         contentType: 'application/json, charset=UTF-8',
+//         success: function(data) {
+//             location.reload();
+//         },
+//         error: function(err) {
+//             console.log(err);
+//         }
+//     });
+// }
